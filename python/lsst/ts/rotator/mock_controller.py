62fd9743f1bf556643669c103f41f23e0fafa24b
Russell Owen <rowen@uw.edu>
2019-10-08 08:49:00 -0700

    Implement more of the mock controller

--- python/lsst/ts/rotator/mock_controller.py
+++ python/lsst/ts/rotator/mock_controller.py
@@ -20,10 +20,18 @@
 
 __all__ = ["MockMTRotatorController"]
 
+import asyncio
 import time
 
+from lsst.ts import salobj
 from lsst.ts import pymoog
+from . import constants
+from . import enums
 from . import structs
+
+# Maximum time between track commands (seconds)
+# The real controller may use 0.15
+TRACK_TIMEOUT = 1
 
 
 class Actuator:
@@ -123,6 +131,18 @@ class MockMTRotatorController(pymoog.BaseMockController):
     To stop the server:
 
         await ctrl.stop()
+
+    *Known Limitations*
+
+    * Constant-velocity motion is not supported.
+    * The path generator is very primitive; acceleration is presently
+      instantaneous. This could be improved by using code from
+      the ATMCS simulator.
+    * I am not sure what the real controller does if a track command is late;
+      for now the simulator goes into FAULT.
+    * I am not sure if the real controller checks that a position was set
+      before accepting the MOVE_POINT_TO_POINT command:
+      for now the simulator does not check.
     """
     def __init__(self, log):
         self.encoder_resolution = 200_000  # counts/deg; arbitrary
@@ -135,6 +155,7 @@ class MockMTRotatorController(pymoog.BaseMockController):
         config.following_error_threshold = 0.1  # deg
         config.track_success_pos_threshold = 0.01  # deg
         config.tracking_lost_timeout = 5  # sec
+        self.tracking_timer_task = salobj.make_done_future()
 
         self.rotator = Actuator(min_pos=config.lower_pos_limit,
                                 max_pos=config.upper_pos_limit,
@@ -151,7 +172,98 @@ class MockMTRotatorController(pymoog.BaseMockController):
         super().close(kill_connect=kill_connect)
 
     async def run_command(self, command):
-        pass
+        if self.telemetry.state == enums.State.OFFLINE:
+            if command.cmd == enums.CommandCode.SET_STATE and command.param1 == enums.State.ENTER_CONTROL:
+                self.telemetry.state = enums.State.STANDBY
+                self.telemetry.enabled_substate = 0
+                self.telemetry.offline_substate = 0
+            elif command.cmd == enums.CommandCode.SET_STATE and command.param1 == enums.State.CLEAR_ERROR:
+                # silently ignore extra CLEAR_ERROR commands
+                pass
+            else:
+                self.log.error(f"Ignoring cmd={command.cmd}; only SET_STATE/ENTER_CONTROL valid in OFFLINE")
+        elif self.telemetry.state == enums.State.STANDBY:
+            if command.cmd == enums.CommandCode.SET_STATE and command.param1 == enums.State.DISABLED:
+                self.telemetry.state = enums.State.DISABLED
+                self.telemetry.enabled_substate = 0
+                self.telemetry.offline_substate = 0
+            else:
+                self.log.error(f"Ignoring cmd={command.cmd}; only SET_STATE/DISABLED valid in STANDBY")
+        elif self.telemetry.state == enums.State.DISABLED:
+            if command.cmd == enums.CommandCode.SET_STATE and command.param1 == enums.State.STANDBY:
+                self.telemetry.state = enums.State.STANDBY
+                self.telemetry.enabled_substate = 0
+                self.telemetry.offline_substate = 0
+            elif command.cmd == enums.CommandCode.SET_STATE and command.param1 == enums.State.START:
+                self.telemetry.state = enums.State.ENABLED
+                self.telemetry.enabled_substate = enums.EnabledSubstate.STATIONARY
+                self.telemetry.offline_substate = 0
+            else:
+                self.log.error(f"Ignoring cmd={command.cmd}; only SET_STATE/DISABLED valid in STANDBY")
+        elif self.telemetry.state == enums.State.FAULT:
+            if command.cmd == enums.CommandCode.SET_STATE and command.param1 == enums.State.CLEAR_ERROR:
+                self.telemetry.state = enums.State.OFFLINE
+                self.telemetry.enabled_substate = 0
+                self.telemetry.offline_substate = enums.OfflineSubState.AVAILABLE
+        elif self.telemetry.state == enums.State.ENABLED:
+            if command.cmd == enums.CommandCode.SET_STATE and command.param1 == enums.State.DISABLED:
+                self.telemetry.state = enums.State.DISABLED
+                self.telemetry.enabled_substate = 0
+                self.telemetry.offline_substate = 0
+            elif self.telemetry.enabled_substate == enums.EnabledSubstate.STATIONARY:
+                if command.cmd == enums.CommandCode.POSITION_SET:
+                    self.telemetry.set_pos = command.param1
+                elif command.cmd == enums.CommandCode.CONFIG_VEL:
+                    if 0 < command.param1 <= constants.MAX_VEL_LIMIT:
+                        self.config.velocity_limit = command.param1
+                    else:
+                        self.log.error(f"Requested velocity limit {command.param1} "
+                                       f"not in range (0, {constants.MAX_VEL_LIMIT}]")
+                elif command.cmd == enums.CommandCode.CONFIG_ACCEL:
+                    if 0 < command.param1 <= constants.MAX_ACCEL_LIMIT:
+                        self.config.accel_limit = command.param1
+                    else:
+                        self.log.error(f"Requested accel limit {command.param1} "
+                                       f"not in range (0, {constants.MAX_ACCEL_LIMIT}]")
+                elif command.cmd == enums.SET_CONSTANT_VEL:
+                    self.log.error("Mock controller does not support constant velocity motion.")
+                elif command.cmd == enums.SET_SUBSTATE:
+                    if command.param1 == enums.EnabledSubstate.TRACK:
+                        self.telemetry.enabled_substate = enums.EnabledSubstate.TRACK
+                        self.tracking_timer_task.cancel()
+                        self.tracking_timer_task = await self.tracking_timer()
+                    elif command.param1 == enums.EnabledSubstate.STOP:
+                        self.rotator.stop()
+                        self.tracking_timer_task.cancel()
+                        self.telemetry.enabled_substate = enums.EnabledSubstate.STATIONARY
+                    elif command.param1 == enums.EnabledSubstate.MOVE_POINT_TO_POINT:
+                        self.rotator.set_pos(self.telemetry.set_pos)
+                        self.telemetry.enabled_substate = enums.EnabledSubstate.MOVE_POINT_TO_POINT
+                        # TODO: output appropriate telemetry and transition to
+                        # STATIONARY when done; do that in the telemetry loop
+                    else:
+                        self.log.error(f"Ignoring unsupported SET SUBSTATE={command.param1} command")
+            elif self.telemetry.enabled_substate == enums.EnabledSubstate.TRACK:
+                if command.cmd == enums.TRACK_VEL_CMD:
+                    tai, pos, vel = command.param1, command.param2, command.param3
+                    dt = salobj.current_tai() - tai
+                    curr_pos = pos + vel*dt
+                    if self.config.lower_pos_limit <= curr_pos <= self.config.upper_pos_limit:
+                        self.rotator.set_pos(curr_pos)
+                        self.tracking_timer_task.cancel()
+                        self.telemetry.enabled_substate = enums.EnabledSubstate.STATIONARY
+                    else:
+                        self.log.error(f"current position {curr_pos} not in range "
+                                       f"[{self.config.lower_pos_limit}, {self.config.upper_pos_limit}]")
+                        self.telemetry.state = enums.State.FAULT
+                        self.telemetry.enabled_substate = 0
+        else:
+            self.log.error(f"Unexpected state {self.telemetry.state}")
+
+    async def tracking_timer(self):
+        await asyncio.sleep(TRACK_TIMEOUT)
+        self.log.error("Tracking timer expired; going to FAULT")
+        self.telemetry.state = enums.State.FAULT
 
     async def update_telemetry(self):
         curr_pos_deg = self.rotator.curr_pos
@@ -172,9 +284,9 @@ class MockMTRotatorController(pymoog.BaseMockController):
         self.telemetry.application_status = 1
         self.telemetry.cmd_pos = cmd_pos_deg
         self.telemetry.set_pos = cmd_pos_deg
-        self.telemetry.state = 1
-        self.telemetry.enabled_substate = 1
-        self.telemetry.offline_substate = 1
+        self.telemetry.state = enums.State.OFFLINE
+        self.telemetry.enabled_substate = 0
+        self.telemetry.offline_substate = enums.OfflineSubstate.AVAILABLE
         self.telemetry.flags_slew_complete = 1
         self.telemetry.flags_pt2pt_move_complete = 1
         self.telemetry.flags_stop_complete = 1
