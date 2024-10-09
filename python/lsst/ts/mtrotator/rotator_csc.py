3e3f1bdb8080d047eb291551610d6fa914f4a4b6
Te-Wei <twtsaia@gmail.com>
2024-10-09 15:16:46 -0300

    Remove the workaround of ts_xml backward compatibility.

--- python/lsst/ts/mtrotator/rotator_csc.py
+++ python/lsst/ts/mtrotator/rotator_csc.py
@@ -230,14 +230,11 @@ class RotatorCsc(hexrotcomm.BaseCsc):
         frequencies = self._vibration_detector.check_vibration_frequency()
 
         for frequency in frequencies:
-            # This is to keep the backward compatibility with ts_xml v22.0.0
-            # that does not have the 'lowFrequencyVibration' defined in xml.
-            if hasattr(self, "evt_lowFrequencyVibration"):
-                await self.evt_lowFrequencyVibration.set_write(frequency=frequency)
-                # EFD has a time window of some milliseconds. Therefore, sleep
-                # a little bit to give it the enough time to avoid the lost
-                # of event.
-                await asyncio.sleep(0.1)
+            await self.evt_lowFrequencyVibration.set_write(frequency=frequency)
+            # EFD has a time window of some milliseconds. Therefore, sleep
+            # a little bit to give it the enough time to avoid the lost
+            # of event.
+            await asyncio.sleep(0.1)
 
     async def close_tasks(self) -> None:
         self.next_clock_offset_task.cancel()
